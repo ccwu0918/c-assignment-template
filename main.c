ff26af10da13b61662647f810432aaaefd79fca4
Chia-Chun Wu <ccwu0918@gmail.com>
2026-03-24 22:55:05 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -3,7 +3,14 @@
 int sum(int a, int b);
 
 int main() {
-    int a = 3, b = 4;
+    int a = 0, b = 0;
+
+    // 讀取兩個整數
+    scanf("%d %d", &a, &b);
+    
+    // 輸出 a+b 的結果
+    // printf("%d\n", a + b);    
+
     printf("Sum: %d\n", sum(a, b));
     return 0;
 }
